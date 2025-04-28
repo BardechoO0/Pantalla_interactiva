e1001a4a9930dd9f5b82907a46824fa82f845c2e
BardechoO0 <bardecho00@gmail.com>
2025-04-28 11:21:49 +0200

    Juego Memoria base
    El juego de memoria es funcional, tiene un inicio y un final aunque no se indique

---
+++ Assets/JuegoMemoria/Scripts/EnemigoPatron.cs
@@ -0,0 +1,246 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class EnemigoPatron : MonoBehaviour
+{
+    private BaseEventData BED;
+
+    public Button triangulo_enemigos;
+    public Button cuadrado_enemigos;
+    public Button circulo_enemigos;
+    public Button X_enemigos;
+
+    public Button triangulo_aliados;
+    public Button cuadrado_aliados;
+    public Button circulo_aliados;
+    public Button X_aliados;
+
+    public int longitudDelJuego;
+    public int cuantoLeQueda;
+
+    public int botonSelecinado;
+    public int[] listaDeNumerosEnemigos;
+    public int[] listaDeNumerosAlidaos;
+
+    public int conteo1;
+    public int conteo2;
+
+    public GameObject[] memoria_enemigos;
+    public GameObject[] memoria_aliados;
+    public GameObject[] objetosDeMemoria;
+
+    public GameObject[] objetos_memoria_enemigos;
+    public GameObject[] objetos_memoria_aliados;
+
+    public bool EstaBien;
+
+    public int Tempo;
+    public Slider Varra_tiempo;
+    public Sprite[] reloj = new Sprite[4];
+    public GameObject imagen_Varra_tiempo;
+    void Start()
+    {
+        triangulo_aliados.interactable = false;
+        cuadrado_aliados.interactable = false;
+        circulo_aliados.interactable = false;
+        X_aliados.interactable = false;
+
+        cuantoLeQueda = longitudDelJuego;
+        
+        conteo1 = -1;
+        conteo2 = -1;
+
+        listaDeNumerosEnemigos = new int[longitudDelJuego];
+        listaDeNumerosAlidaos = new int[longitudDelJuego];
+
+        objetos_memoria_aliados = new GameObject[longitudDelJuego];
+        objetos_memoria_enemigos = new GameObject[longitudDelJuego];
+
+        Simon();
+
+
+        Varra_tiempo.maxValue = Tempo;
+        Varra_tiempo.value = Tempo;
+        imagen_Varra_tiempo.GetComponent<Image>().sprite = reloj[0];
+        
+    }
+
+    public void Simon()
+    {
+        //Instantiate(objetosDeMemoria[0], memoria_enemigos[conteo].gameObject.transform.position, Quaternion.identity);
+
+
+        if (cuantoLeQueda > 0) 
+        {
+            StartCoroutine(Conteo());
+            listaDeNumerosEnemigos[conteo1] = botonSelecinado;
+            objetos_memoria_enemigos[conteo1] = Instantiate(objetosDeMemoria[botonSelecinado-1], memoria_enemigos[conteo1].gameObject.transform.position, Quaternion.identity);
+        }
+        else
+        {
+            StartCoroutine(tiempo(Tempo));
+            print("Porque te tatuansti");
+        }
+
+        
+    }
+    IEnumerator tiempo(int t)
+    {
+        yield return new WaitForSeconds(1);
+
+
+        //Esto no tiene sentido y se puede ahorar pasos pero me hizo bastante gracia y se queda así
+
+        t = Tempo;
+        t--;
+        Varra_tiempo.value = t;
+
+        imagen_Varra_tiempo.GetComponent<Image>().sprite = reloj[Random.Range(1,4)];
+
+        Tempo = t;
+
+        if (Tempo <= 0)
+        {
+            Empezar();
+        }
+        else
+        {
+           StartCoroutine(tiempo(Tempo));
+        }
+
+    }
+    IEnumerator Conteo()
+    {
+        botonSelecinado = Random.Range(1, 5);
+
+       
+
+       
+        
+        conteo1++;
+
+        if (botonSelecinado == 1) 
+        {
+            triangulo_enemigos.OnSubmit(BED);
+        }
+        else if (botonSelecinado == 2) 
+        {
+            cuadrado_enemigos.OnSubmit(BED);
+        }
+        else if (botonSelecinado == 3) 
+        {
+            circulo_enemigos.OnSubmit(BED);
+        }
+        else if (botonSelecinado == 4 ) 
+        {
+            X_enemigos.OnSubmit(BED);
+        }
+        else 
+        {
+            print("Ella me dice ola y yo le digo godbye");
+        }
+
+        yield return new WaitForSeconds(1f);
+
+        cuantoLeQueda--;
+        Simon();
+    }
+
+
+    public void Empezar() 
+    {
+        triangulo_aliados.interactable = true;
+        cuadrado_aliados.interactable = true;
+        circulo_aliados.interactable = true;
+        X_aliados.interactable = true;
+
+        triangulo_enemigos.interactable = false;
+        cuadrado_enemigos.interactable = false;
+        circulo_enemigos.interactable = false;
+        X_enemigos.interactable = false;
+
+
+    }
+
+    public void checker()
+    {
+        for (int i = 0; i <= conteo2; i++) 
+        {
+            if (listaDeNumerosAlidaos[i] == listaDeNumerosEnemigos[i])
+            {
+                print("Teto>>>>>>>Miku");
+            }
+            else
+            {
+                EstaBien = false;
+            }
+
+        }
+
+        if (EstaBien)
+        {
+            Debug.Log("El print esta bien basado");
+        }
+    }
+    
+    public void triangulo()
+    {
+        conteo2++;
+        listaDeNumerosAlidaos[conteo2] = 1;
+        objetos_memoria_aliados[conteo2] = Instantiate(objetosDeMemoria[0], memoria_aliados[conteo2].gameObject.transform.position, Quaternion.identity);
+
+        if (conteo2 == conteo1) 
+        {
+            checker();
+            print("Y allá en el tambo todo el mundo me decía ¡Ya te vas a mejorar!");
+        }
+    }
+
+    public void cuadrado()
+    {
+        conteo2++;
+        listaDeNumerosAlidaos[conteo2] = 2;
+        objetos_memoria_aliados[conteo2] = Instantiate(objetosDeMemoria[1], memoria_aliados[conteo2].gameObject.transform.position, Quaternion.identity);
+
+        if (conteo2 == conteo1)
+        {
+            checker();
+            print("Y en la familia todo el mundo me decía ¡Ya te vas a mejorar!");
+        }
+    }
+
+    public void circulo() 
+    {
+        conteo2++;
+        listaDeNumerosAlidaos[conteo2] = 3;
+        objetos_memoria_aliados[conteo2] = Instantiate(objetosDeMemoria[2], memoria_aliados[conteo2].gameObject.transform.position, Quaternion.identity);
+
+        if (conteo2 == conteo1)
+        {
+            checker();
+            print("Ya perdí la paciencia en la lobotomía ¡Ya te vas a mejorar!");
+        }
+    }
+
+    public void X()
+    {
+        conteo2++;
+        listaDeNumerosAlidaos[conteo2] = 4;
+        objetos_memoria_aliados[conteo2] = Instantiate(objetosDeMemoria[3], memoria_aliados[conteo2].gameObject.transform.position, Quaternion.identity);
+
+        if (conteo2 == conteo1)
+        {
+            checker();
+            print("Perdí mi mano izquierda y me duele todavía ¡Ya te vas a mejorar!");
+        }
+    }
+    void Update()
+    {
+        
+    }
+}
